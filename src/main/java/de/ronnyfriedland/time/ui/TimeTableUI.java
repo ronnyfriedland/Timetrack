b49eec461be606e0df813669f624f2d8ee14f8d8
Ronny Friedland <mail@ronnyfriedland.de>
2012-03-27 20:06:08 +0200

    Doppelklick öffnet Dialog "neuer Eintrag"

--- src/main/java/de/ronnyfriedland/time/ui/TimeTableUI.java
+++ src/main/java/de/ronnyfriedland/time/ui/TimeTableUI.java
@@ -38,145 +38,151 @@ import de.ronnyfriedland.time.ui.dialog.NewProjectFrame;
  */
 public class TimeTableUI {
 
-    private static final Logger LOG = Logger.getLogger(TimeTableUI.class.getName());
+	private static final Logger LOG = Logger.getLogger(TimeTableUI.class.getName());
 
-    public static void main(final String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            /**
-             * (non-Javadoc)
-             * 
-             * @see java.lang.Runnable#run()
-             */
-            @Override
-            public void run() {
-                new TimeTableUI().createAndShowGUI();
-            }
-        });
-    }
+	public static void main(final String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			/**
+			 * (non-Javadoc)
+			 * 
+			 * @see java.lang.Runnable#run()
+			 */
+			@Override
+			public void run() {
+				new TimeTableUI().createAndShowGUI();
+			}
+		});
+	}
 
-    private void createAndShowGUI() {
-        // Check the SystemTray support
-        if (!SystemTray.isSupported()) {
-            LOG.severe("SystemTray not supported!");
-        } else {
-            final PopupMenu popup = new PopupMenu();
-            final TrayIcon trayIcon = new TrayIcon((new ImageIcon(Thread.currentThread().getContextClassLoader()
-                    .getResource("images/icon.gif"))).getImage());
-            final SystemTray tray = SystemTray.getSystemTray();
+	private void createAndShowGUI() {
+		// Check the SystemTray support
+		if (!SystemTray.isSupported()) {
+			LOG.severe("SystemTray not supported!");
+		} else {
+			final PopupMenu popup = new PopupMenu();
+			final TrayIcon trayIcon = new TrayIcon((new ImageIcon(Thread.currentThread().getContextClassLoader()
+			        .getResource("images/icon.gif"))).getImage());
+			final SystemTray tray = SystemTray.getSystemTray();
 
-            // Create a popup menu components
-            final MenuItem newProject = new MenuItem(Messages.NEW_PROJECT.getText());
-            final MenuItem newItem = new MenuItem(Messages.NEW_ENTRY.getText());
-            final Menu todayItems = new Menu(Messages.LAST_ENTRIES.getText());
-            final MenuItem exportItem = new MenuItem(Messages.EXPORT_DATA.getText());
-            final MenuItem exitItem = new MenuItem(Messages.EXIT.getText());
+			// Create a popup menu components
+			final MenuItem newProject = new MenuItem(Messages.NEW_PROJECT.getText());
+			final MenuItem newItem = new MenuItem(Messages.NEW_ENTRY.getText());
+			final Menu todayItems = new Menu(Messages.LAST_ENTRIES.getText());
+			final MenuItem exportItem = new MenuItem(Messages.EXPORT_DATA.getText());
+			final MenuItem exitItem = new MenuItem(Messages.EXIT.getText());
 
-            // Add components to popup menu
-            popup.add(newProject);
-            popup.add(newItem);
-            popup.addSeparator();
-            popup.add(todayItems);
-            popup.add(exportItem);
-            popup.addSeparator();
-            popup.add(exitItem);
+			// Add components to popup menu
+			popup.add(newProject);
+			popup.add(newItem);
+			popup.addSeparator();
+			popup.add(todayItems);
+			popup.add(exportItem);
+			popup.addSeparator();
+			popup.add(exitItem);
 
-            trayIcon.setPopupMenu(popup);
-            trayIcon.setToolTip(Messages.TITLE.getText());
+			trayIcon.setPopupMenu(popup);
+			trayIcon.setToolTip(Messages.TITLE.getText());
 
-            trayIcon.addMouseListener(new MouseListener() {
+			trayIcon.addMouseListener(new MouseListener() {
 
-                @Override
-                public void mouseReleased(MouseEvent e) {
-                }
+				@Override
+				public void mouseReleased(MouseEvent e) {
+				}
 
-                @Override
-                public void mousePressed(MouseEvent e) {
-                    Collection<Entry> todayEntries = EntityController.getInstance().findAll(Entry.class,
-                            new SortParam("date", SortOrder.DESC), 10);
+				@Override
+				public void mousePressed(MouseEvent e) {
+					if ((MouseEvent.BUTTON1 == e.getButton())) {
+						if (2 == e.getClickCount()) {
+							new NewEntryFrame().setVisible(true);
+						}
+					} else {
+						Collection<Entry> todayEntries = EntityController.getInstance().findAll(Entry.class,
+						        new SortParam("date", SortOrder.DESC), 10);
 
-                    todayItems.removeAll();
+						todayItems.removeAll();
 
-                    for (final Entry entry : todayEntries) {
-                        MenuItem menuItem = new MenuItem(String.format("%1$s: %2$sh - %3$s (%4$s)", entry
-                                .getDateString(), entry.getDuration(), entry.getDescription(), entry.getProject()
-                                .getName()));
-                        menuItem.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent e) {
-                                new NewEntryFrame(entry).setVisible(true);
-                            }
-                        });
-                        todayItems.add(menuItem);
-                    }
-                }
+						for (final Entry entry : todayEntries) {
+							MenuItem menuItem = new MenuItem(String.format("%1$s: %2$sh - %3$s (%4$s)", entry
+							        .getDateString(), entry.getDuration(), entry.getDescription(), entry.getProject()
+							        .getName()));
+							menuItem.addActionListener(new ActionListener() {
+								@Override
+								public void actionPerformed(ActionEvent e) {
+									new NewEntryFrame(entry).setVisible(true);
+								}
+							});
+							todayItems.add(menuItem);
+						}
+					}
+				}
 
-                @Override
-                public void mouseExited(MouseEvent e) {
-                }
+				@Override
+				public void mouseExited(MouseEvent e) {
+				}
 
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                }
+				@Override
+				public void mouseEntered(MouseEvent e) {
+				}
 
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                }
-            });
+				@Override
+				public void mouseClicked(MouseEvent e) {
+				}
+			});
 
-            try {
-                UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+			try {
+				UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 
-                tray.add(trayIcon);
+				tray.add(trayIcon);
 
-                newProject.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        new NewProjectFrame().setVisible(true);
-                    }
-                });
+				newProject.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(final ActionEvent e) {
+						new NewProjectFrame().setVisible(true);
+					}
+				});
 
-                newItem.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        new NewEntryFrame().setVisible(true);
-                    }
-                });
+				newItem.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(final ActionEvent e) {
+						new NewEntryFrame().setVisible(true);
+					}
+				});
 
-                exportItem.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        new ExportFrame().setVisible(true);
-                    }
-                });
+				exportItem.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(final ActionEvent e) {
+						new ExportFrame().setVisible(true);
+					}
+				});
 
-                exitItem.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(final ActionEvent e) {
-                        try {
-                            QuartzController.getInstance().shutdownScheduler();
-                        } catch (SchedulerException ex) {
-                            LOG.log(Level.SEVERE, "Error shutting down scheduler.", ex);
-                        }
-                        tray.remove(trayIcon);
-                        System.exit(0);
-                    }
-                });
+				exitItem.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(final ActionEvent e) {
+						try {
+							QuartzController.getInstance().shutdownScheduler();
+						} catch (SchedulerException ex) {
+							LOG.log(Level.SEVERE, "Error shutting down scheduler.", ex);
+						}
+						tray.remove(trayIcon);
+						System.exit(0);
+					}
+				});
 
-                // initialize Controller ...
-                EntityController.getInstance();
-                QuartzController.getInstance().initScheduler(
-                        Configurator.CONFIG.getString(ConfiguratorKeys.CRON_EXPRESSION_POPUP.getKey()));
+				// initialize Controller ...
+				EntityController.getInstance();
+				QuartzController.getInstance().initScheduler(
+				        Configurator.CONFIG.getString(ConfiguratorKeys.CRON_EXPRESSION_POPUP.getKey()));
 
-                LogManager logManager = LogManager.getLogManager();
-                try {
-                    logManager.readConfiguration(Thread.currentThread().getContextClassLoader()
-                            .getResourceAsStream("logging.properties"));
-                } catch (IOException e) {
-                    LOG.log(Level.SEVERE, "Error reading logging.properties to configure logger.", e);
-                }
-            } catch (Exception e) {
-                LOG.log(Level.SEVERE, "Unable to set look and feel or add tray icon", e);
-            }
-        }
-    }
+				LogManager logManager = LogManager.getLogManager();
+				try {
+					logManager.readConfiguration(Thread.currentThread().getContextClassLoader()
+					        .getResourceAsStream("logging.properties"));
+				} catch (IOException e) {
+					LOG.log(Level.SEVERE, "Error reading logging.properties to configure logger.", e);
+				}
+			} catch (Exception e) {
+				LOG.log(Level.SEVERE, "Unable to set look and feel or add tray icon", e);
+			}
+		}
+	}
 }
