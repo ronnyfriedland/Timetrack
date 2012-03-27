7991e7f5b63923201f24ef274c2a6c1274575349
Ronny Friedland <mail@ronnyfriedland.de>
2012-03-27 20:49:43 +0200

    Kopieren der Daten in Zwischenablage umgebaut #2

--- src/main/java/de/ronnyfriedland/time/ui/adapter/TimeTableKeyAdapter.java
+++ src/main/java/de/ronnyfriedland/time/ui/adapter/TimeTableKeyAdapter.java
@@ -7,47 +7,55 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 
 /**
  * @author ronnyfriedland
  */
 public class TimeTableKeyAdapter extends KeyAdapter {
-    /**
-     * (non-Javadoc)
-     * 
-     * @see java.awt.event.KeyAdapter#keyPressed(java.awt.event.KeyEvent)
-     */
-    @Override
-    public void keyPressed(KeyEvent e) {
-    	switch(e.getKeyCode()) {
-    	case KeyEvent.VK_ESCAPE:
-            SwingUtilities.getRoot(e.getComponent()).setVisible(false);
-    		break;
-    	case KeyEvent.VK_C:
-    		if(e.isControlDown()) {
-    			if(e.getComponent() instanceof JTable) {
-        			//copySelectedValue((JTable)e.getComponent());
-    			}
-    		}
-    		break;
-    	default:
-    		// not implemented yet
-    		break;
-    	}
-    }
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see java.awt.event.KeyAdapter#keyPressed(java.awt.event.KeyEvent)
+	 */
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_ESCAPE:
+			SwingUtilities.getRoot(e.getComponent()).setVisible(false);
+			break;
+		case KeyEvent.VK_C:
+			if (e.isControlDown()) {
+				if (e.getComponent() instanceof JTable) {
+					copySelectedValue((JTable) e.getComponent());
+				}
+			}
+			break;
+		default:
+			// not implemented yet
+			break;
+		}
+	}
 
-    /**
-     * Kopiert die ausgewählten Daten in die Zwischenablage
-     */
-    private void copySelectedValue(final JTable table) {
-    	int col = table.getSelectedColumn();
-    	int row = table.getSelectedRow();
-    	String value = (String)table.getValueAt(row, col);
-    	
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-        StringSelection data = new StringSelection(value);
-        clipboard.setContents(data, data);
-    }
+	/**
+	 * Kopiert die ausgewählten Daten in die Zwischenablage
+	 */
+	private void copySelectedValue(final JTable table) {
+		ListSelectionModel selectedData = table.getSelectionModel();
+		int min = selectedData.getMinSelectionIndex();
+		int max = selectedData.getMaxSelectionIndex();
+
+		StringBuilder sbuild = new StringBuilder();
+		if (-1 < min && -1 < max) {
+			for (int i = min; i <= max; i++) {
+				sbuild.append(table.getValueAt(i, 1));
+				sbuild.append(" (").append(table.getValueAt(i, 2)).append(") : ");
+				sbuild.append(table.getValueAt(i, 3)).append(" h\n");
+			}
+		}
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(new StringSelection(sbuild.toString()), null);
+	}
 
 }
--- src/main/java/de/ronnyfriedland/time/ui/dialog/ExportFrame.java
+++ src/main/java/de/ronnyfriedland/time/ui/dialog/ExportFrame.java
@@ -23,6 +23,7 @@ import javax.swing.JSlider;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableModel;
@@ -44,228 +45,230 @@ import de.ronnyfriedland.time.ui.adapter.TimeTableKeyAdapter;
  * @author ronnyfriedland
  */
 public class ExportFrame extends AbstractFrame {
-    private static final Logger LOG = Logger.getLogger(ExportFrame.class.getName());
+	private static final Logger LOG = Logger.getLogger(ExportFrame.class.getName());
 
-    private static final long serialVersionUID = -8738367859388084898L;
+	private static final long serialVersionUID = -8738367859388084898L;
 
-    private static final String LABEL_SELECTED_DAYS_VALUE = "%1$d Tag(e)";
-    private static final String LABEL_SELECTED_EXPORT_DATA = "%1$.2f Stunden für %2$d Tage ausgewählt.";
+	private static final String LABEL_SELECTED_DAYS_VALUE = "%1$d Tag(e)";
+	private static final String LABEL_SELECTED_EXPORT_DATA = "%1$.2f Stunden für %2$d Tage ausgewählt.";
 
-    private static final String[] TABLE_HEADERS = new String[] { Messages.DATE.getText(),
-            Messages.DESCRIPTION.getText(), Messages.PROJECT_NAME.getText(), Messages.DURATION.getText() };
+	private static final String[] TABLE_HEADERS = new String[] { Messages.DATE.getText(),
+	        Messages.DESCRIPTION.getText(), Messages.PROJECT_NAME.getText(), Messages.DURATION.getText() };
 
-    private final JLabel labelDate = new JLabel(Messages.START_DATE.getText());
-    private final DateChooserPanel dateChooser = new DateChooserPanel();
-    private final JLabel labelDays = new JLabel(Messages.PERIOD_OF_TIME.getText());
-    private final JSlider days = new JSlider(JSlider.HORIZONTAL, 1, 365, 7);
-    private final JLabel labelSelectedDays = new JLabel("");
-    private final JButton preview = new JButton(Messages.PREVIEW.getText());
-    private final JButton export = new JButton(Messages.EXPORT.getText());
-    private final JLabel summary = new JLabel();
-    private final JTextArea description = new JTextArea();
-    private final DefaultTableModel tableModel = new DefaultTableModel(TABLE_HEADERS, 0) {
-        private static final long serialVersionUID = 2177197508177608415L;
+	private final JLabel labelDate = new JLabel(Messages.START_DATE.getText());
+	private final DateChooserPanel dateChooser = new DateChooserPanel();
+	private final JLabel labelDays = new JLabel(Messages.PERIOD_OF_TIME.getText());
+	private final JSlider days = new JSlider(JSlider.HORIZONTAL, 1, 365, 7);
+	private final JLabel labelSelectedDays = new JLabel("");
+	private final JButton preview = new JButton(Messages.PREVIEW.getText());
+	private final JButton export = new JButton(Messages.EXPORT.getText());
+	private final JLabel summary = new JLabel();
+	private final JTextArea description = new JTextArea();
+	private final DefaultTableModel tableModel = new DefaultTableModel(TABLE_HEADERS, 0) {
+		private static final long serialVersionUID = 2177197508177608415L;
 
-        @Override
-        public boolean isCellEditable(int row, int column) {
-            return false;
-        };
-    };
-    private final TableCellRenderer tableCellRenderer = new TableCellRenderer() {
-    	@Override
-        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                boolean hasFocus, int row, int column) {
-            JTextField component = new JTextField();
-            if (row % 2 == 0) {
-                component.setBackground(Color.WHITE);
-            } else {
-                component.setBackground(Color.LIGHT_GRAY);
-            }
-            if (isSelected) {
-                component.setBackground(Color.GRAY);
-                component.setForeground(Color.WHITE);
-            } else {
-                component.setForeground(Color.BLACK);
-            }
-            component.setText(value.toString());
-            component.setToolTipText(value.toString());
-            component.setBorder(BorderFactory.createEmptyBorder());
-            return component;
-        }
-    };
-    private final JTable table = new JTable();
-    {
-        table.setModel(tableModel);
-        table.setDefaultRenderer(Object.class, tableCellRenderer);
-        table.addKeyListener(new TimeTableKeyAdapter());
-    }
-    private final JScrollPane scrollPane = new JScrollPane(table);
+		@Override
+		public boolean isCellEditable(int row, int column) {
+			return false;
+		};
+	};
+	private final TableCellRenderer tableCellRenderer = new TableCellRenderer() {
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+		        boolean hasFocus, int row, int column) {
+			JTextField component = new JTextField();
+			if (row % 2 == 0) {
+				component.setBackground(Color.WHITE);
+			} else {
+				component.setBackground(Color.LIGHT_GRAY);
+			}
+			if (isSelected) {
+				component.setBackground(Color.GRAY);
+				component.setForeground(Color.WHITE);
+			} else {
+				component.setForeground(Color.BLACK);
+			}
+			component.setText(value.toString());
+			component.setToolTipText(value.toString());
+			component.setBorder(BorderFactory.createEmptyBorder());
+			return component;
+		}
+	};
+	private final JTable table = new JTable();
+	{
+		table.setModel(tableModel);
+		table.setDefaultRenderer(Object.class, tableCellRenderer);
+		table.addKeyListener(new TimeTableKeyAdapter());
+		table.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		table.setTransferHandler(null); // remove default TransferHandler
+	}
+	private final JScrollPane scrollPane = new JScrollPane(table);
 
-    public ExportFrame() {
-        super(Messages.NEW_EXPORT.getText(), 500, 460);
-        createUI();
-    }
+	public ExportFrame() {
+		super(Messages.NEW_EXPORT.getText(), 500, 460);
+		createUI();
+	}
 
-    /**
-     * (non-Javadoc)
-     * 
-     * @see de.ronnyfriedland.time.ui.dialog.AbstractFrame#createUI()
-     */
-    @Override
-    protected void createUI() {
-        // configure
-        labelDate.setBounds(10, 10, 100, 24);
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see de.ronnyfriedland.time.ui.dialog.AbstractFrame#createUI()
+	 */
+	@Override
+	protected void createUI() {
+		// configure
+		labelDate.setBounds(10, 10, 100, 24);
 
-        dateChooser.setBounds(110, 10, 200, 150);
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.DAY_OF_WEEK, 2);
-        dateChooser.setDate(cal.getTime());
-        dateChooser.setChosenDateButtonColor(Color.LIGHT_GRAY);
-        dateChooser.setYearSelectionRange(2);
-        dateChooser.setBorder(BorderFactory.createEmptyBorder());
-        dateChooser.addKeyListener(new TimeTableKeyAdapter());
+		dateChooser.setBounds(110, 10, 200, 150);
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_WEEK, 2);
+		dateChooser.setDate(cal.getTime());
+		dateChooser.setChosenDateButtonColor(Color.LIGHT_GRAY);
+		dateChooser.setYearSelectionRange(2);
+		dateChooser.setBorder(BorderFactory.createEmptyBorder());
+		dateChooser.addKeyListener(new TimeTableKeyAdapter());
 
-        labelDays.setBounds(10, 170, 100, 24);
+		labelDays.setBounds(10, 170, 100, 24);
 
-        days.setBounds(110, 170, 200, 24);
-        days.setValue(7);
-        days.addKeyListener(new TimeTableKeyAdapter());
-        days.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(final ChangeEvent e) {
-                JSlider source = (JSlider) e.getSource();
-                labelSelectedDays.setText(String.format(LABEL_SELECTED_DAYS_VALUE, source.getValue()));
-            }
-        });
+		days.setBounds(110, 170, 200, 24);
+		days.setValue(7);
+		days.addKeyListener(new TimeTableKeyAdapter());
+		days.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(final ChangeEvent e) {
+				JSlider source = (JSlider) e.getSource();
+				labelSelectedDays.setText(String.format(LABEL_SELECTED_DAYS_VALUE, source.getValue()));
+			}
+		});
 
-        labelSelectedDays.setBounds(110, 200, 200, 24);
-        labelSelectedDays.setText(String.format(LABEL_SELECTED_DAYS_VALUE, days.getValue()));
+		labelSelectedDays.setBounds(110, 200, 200, 24);
+		labelSelectedDays.setText(String.format(LABEL_SELECTED_DAYS_VALUE, days.getValue()));
 
-        export.setBounds(170, 225, 140, 24);
-        export.addKeyListener(new TimeTableKeyAdapter());
-        export.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                try {
-                    Calendar from = getStartDate();
-                    Calendar to = getEndDate();
-                    Collection<Entry> entries = getFilteredData(from, to);
+		export.setBounds(170, 225, 140, 24);
+		export.addKeyListener(new TimeTableKeyAdapter());
+		export.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				try {
+					Calendar from = getStartDate();
+					Calendar to = getEndDate();
+					Collection<Entry> entries = getFilteredData(from, to);
 
-                    ExportController controller = new ExportController();
-                    Workbook wb = controller.loadOrCreateWorkbook(
-                            Configurator.CONFIG.getString(ConfiguratorKeys.PATH.getKey()),
-                            Configurator.CONFIG.getString(ConfiguratorKeys.EXPORT_FILE.getKey()));
-                    Sheet sheet = controller.loadOrCreateSheet(wb, SimpleDateFormat.getDateInstance(DateFormat.SHORT)
-                            .format(from.getTime()), entries);
-                    controller.addSheetToOverview(wb, sheet.getSheetName());
-                    controller.persistWorkbook(wb, Configurator.CONFIG.getString(ConfiguratorKeys.PATH.getKey()),
-                            Configurator.CONFIG.getString(ConfiguratorKeys.EXPORT_FILE.getKey()));
+					ExportController controller = new ExportController();
+					Workbook wb = controller.loadOrCreateWorkbook(
+					        Configurator.CONFIG.getString(ConfiguratorKeys.PATH.getKey()),
+					        Configurator.CONFIG.getString(ConfiguratorKeys.EXPORT_FILE.getKey()));
+					Sheet sheet = controller.loadOrCreateSheet(wb, SimpleDateFormat.getDateInstance(DateFormat.SHORT)
+					        .format(from.getTime()), entries);
+					controller.addSheetToOverview(wb, sheet.getSheetName());
+					controller.persistWorkbook(wb, Configurator.CONFIG.getString(ConfiguratorKeys.PATH.getKey()),
+					        Configurator.CONFIG.getString(ConfiguratorKeys.EXPORT_FILE.getKey()));
 
-                    setVisible(false);
+					setVisible(false);
 
-                    JOptionPane.showMessageDialog(
-                            null,
-                            String.format(Messages.EXPORT_SUCCESSFUL.getText(),
-                                    Configurator.CONFIG.getString(ConfiguratorKeys.PATH.getKey())));
-                } catch (IOException ex) {
-                    LOG.log(Level.SEVERE, "Error exporting data", ex);
-                    formatError(export);
-                }
+					JOptionPane.showMessageDialog(
+					        null,
+					        String.format(Messages.EXPORT_SUCCESSFUL.getText(),
+					                Configurator.CONFIG.getString(ConfiguratorKeys.PATH.getKey())));
+				} catch (IOException ex) {
+					LOG.log(Level.SEVERE, "Error exporting data", ex);
+					formatError(export);
+				}
 
-            }
-        });
+			}
+		});
 
-        preview.setBounds(10, 225, 140, 24);
-        preview.addKeyListener(new TimeTableKeyAdapter());
-        preview.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                tableModel.setDataVector(new Object[0][0], TABLE_HEADERS);
+		preview.setBounds(10, 225, 140, 24);
+		preview.addKeyListener(new TimeTableKeyAdapter());
+		preview.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				tableModel.setDataVector(new Object[0][0], TABLE_HEADERS);
 
-                Calendar from = getStartDate();
-                Calendar to = getEndDate();
-                Collection<Entry> entries = getFilteredData(from, to);
+				Calendar from = getStartDate();
+				Calendar to = getEndDate();
+				Collection<Entry> entries = getFilteredData(from, to);
 
-                float hours = 0;
-                for (Entry entry : entries) {
-                    Float duration = Float.valueOf(entry.getDuration());
-                    hours += duration;
-                    tableModel.addRow(new Object[] { entry.getDateString(), entry.getDescription(),
-                            entry.getProject().getName(), String.format("%1$.2f", duration) });
-                }
+				float hours = 0;
+				for (Entry entry : entries) {
+					Float duration = Float.valueOf(entry.getDuration());
+					hours += duration;
+					tableModel.addRow(new Object[] { entry.getDateString(), entry.getDescription(),
+					        entry.getProject().getName(), String.format("%1$.2f", duration) });
+				}
 
-                summary.setText(String.format(LABEL_SELECTED_EXPORT_DATA, hours, days.getValue()));
-            }
-        });
+				summary.setText(String.format(LABEL_SELECTED_EXPORT_DATA, hours, days.getValue()));
+			}
+		});
 
-        scrollPane.setBounds(10, 260, 480, 150);
+		scrollPane.setBounds(10, 260, 480, 150);
 
-        summary.setBounds(10, 410, 480, 20);
+		summary.setBounds(10, 410, 480, 20);
 
-        description.setBounds(320, 10, 170, 240);
-        description.setEditable(false);
-        description.setLineWrap(true);
-        description.setWrapStyleWord(true);
-        description.setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        description.setText(Messages.EXPORT_DESCRIPTION.getText());
+		description.setBounds(320, 10, 170, 240);
+		description.setEditable(false);
+		description.setLineWrap(true);
+		description.setWrapStyleWord(true);
+		description.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+		description.setText(Messages.EXPORT_DESCRIPTION.getText());
 
-        formatOk(dateChooser, days);
+		formatOk(dateChooser, days);
 
-        getContentPane().add(labelDate);
-        getContentPane().add(dateChooser);
-        getContentPane().add(labelDays);
-        getContentPane().add(days);
-        getContentPane().add(labelSelectedDays);
-        getContentPane().add(scrollPane);
-        getContentPane().add(preview);
-        getContentPane().add(export);
-        getContentPane().add(summary);
-        getContentPane().add(description);
-    }
+		getContentPane().add(labelDate);
+		getContentPane().add(dateChooser);
+		getContentPane().add(labelDays);
+		getContentPane().add(days);
+		getContentPane().add(labelSelectedDays);
+		getContentPane().add(scrollPane);
+		getContentPane().add(preview);
+		getContentPane().add(export);
+		getContentPane().add(summary);
+		getContentPane().add(description);
+	}
 
-    /**
-     * Liefert das Anfangsdatum für den Export.
-     * 
-     * @return Calendar
-     */
-    private Calendar getStartDate() {
-        Calendar from = Calendar.getInstance();
-        from.setTime(dateChooser.getDate());
-        from.set(Calendar.HOUR_OF_DAY, 0);
-        from.set(Calendar.MINUTE, 0);
-        from.set(Calendar.SECOND, 0);
-        from.set(Calendar.MILLISECOND, 0);
-        return from;
-    }
+	/**
+	 * Liefert das Anfangsdatum für den Export.
+	 * 
+	 * @return Calendar
+	 */
+	private Calendar getStartDate() {
+		Calendar from = Calendar.getInstance();
+		from.setTime(dateChooser.getDate());
+		from.set(Calendar.HOUR_OF_DAY, 0);
+		from.set(Calendar.MINUTE, 0);
+		from.set(Calendar.SECOND, 0);
+		from.set(Calendar.MILLISECOND, 0);
+		return from;
+	}
 
-    /**
-     * Liefert das Enddatum für den Export.
-     * 
-     * @return Calendar
-     */
-    private Calendar getEndDate() {
-        Calendar to = Calendar.getInstance();
-        to.setTime(dateChooser.getDate());
-        to.add(Calendar.DAY_OF_YEAR, days.getValue());
-        to.set(Calendar.HOUR_OF_DAY, 0);
-        to.set(Calendar.MINUTE, 0);
-        to.set(Calendar.SECOND, 0);
-        to.set(Calendar.MILLISECOND, 0);
-        return to;
-    }
+	/**
+	 * Liefert das Enddatum für den Export.
+	 * 
+	 * @return Calendar
+	 */
+	private Calendar getEndDate() {
+		Calendar to = Calendar.getInstance();
+		to.setTime(dateChooser.getDate());
+		to.add(Calendar.DAY_OF_YEAR, days.getValue());
+		to.set(Calendar.HOUR_OF_DAY, 0);
+		to.set(Calendar.MINUTE, 0);
+		to.set(Calendar.SECOND, 0);
+		to.set(Calendar.MILLISECOND, 0);
+		return to;
+	}
 
-    /**
-     * Liefert die gespeicherten Daten anhand des Filters
-     * 
-     * @return Liste der Einträge
-     */
-    private Collection<Entry> getFilteredData(Calendar from, Calendar to) {
-        Map<String, Object> params = new HashMap<String, Object>();
+	/**
+	 * Liefert die gespeicherten Daten anhand des Filters
+	 * 
+	 * @return Liste der Einträge
+	 */
+	private Collection<Entry> getFilteredData(Calendar from, Calendar to) {
+		Map<String, Object> params = new HashMap<String, Object>();
 
-        params.put(Entry.PARAM_DATE_FROM, from.getTime());
-        params.put(Entry.PARAM_DATE_TO, to.getTime());
+		params.put(Entry.PARAM_DATE_FROM, from.getTime());
+		params.put(Entry.PARAM_DATE_TO, to.getTime());
 
-        return EntityController.getInstance().findResultlistByParameter(Entry.class, Entry.QUERY_FIND_FROM_TO, params);
-    }
+		return EntityController.getInstance().findResultlistByParameter(Entry.class, Entry.QUERY_FIND_FROM_TO, params);
+	}
 
 }
