6dcf697736328652236801a8b773a928e08dca5b
Ronny Friedland <mail@ronnyfriedland.de>
2012-07-18 20:35:43 +0200

    checkstyle (tab/spaces)

--- src/main/java/de/ronnyfriedland/time/config/MessageProperties.java
+++ src/main/java/de/ronnyfriedland/time/config/MessageProperties.java
@@ -8,35 +8,35 @@ import java.util.logging.Logger;
  * @author Ronny Friedland
  */
 public final class MessageProperties {
-	/** The logger for {@link MessageProperties} */
-	private static final Logger LOG = Logger.getLogger(MessageProperties.class.getName());
+    /** The logger for {@link MessageProperties} */
+    private static final Logger LOG = Logger.getLogger(MessageProperties.class.getName());
 
-	/**
-	 * Erzeugt eine Instanz mit den angegebenen Werten.
-	 */
-	private MessageProperties() {
-		// empty
-	}
+    /**
+     * Erzeugt eine Instanz mit den angegebenen Werten.
+     */
+    private MessageProperties() {
+        // empty
+    }
 
-	/**
-	 * Liefert den Text zum angegebenen Message-Key für das angegebene Locale.
-	 * 
-	 * @param key
-	 *            der Message-Key
-	 * @param values
-	 *            die (optionalen) Parameter
-	 * @return der Text
-	 */
-	public static String getString(final String key, final String... values) {
-		String result = '!' + key + '!';
-		try {
-			result = PropertyResourceBundle.getBundle("messages").getString(key);
-			if (values != null) {
-				result = String.format(result, values);
-			}
-		} catch (MissingResourceException e) {
-			LOG.info("Bundle für Textbausteine nicht gefunden! " + e);
-		}
-		return result;
-	}
+    /**
+     * Liefert den Text zum angegebenen Message-Key für das angegebene Locale.
+     * 
+     * @param key
+     *            der Message-Key
+     * @param values
+     *            die (optionalen) Parameter
+     * @return der Text
+     */
+    public static String getString(final String key, final String... values) {
+        String result = '!' + key + '!';
+        try {
+            result = PropertyResourceBundle.getBundle("messages").getString(key);
+            if (values != null) {
+                result = String.format(result, values);
+            }
+        } catch (MissingResourceException e) {
+            LOG.info("Bundle für Textbausteine nicht gefunden! " + e);
+        }
+        return result;
+    }
 }
