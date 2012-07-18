100c3056c0db51080a467ad7e09be668dfc67727
Ronny Friedland <mail@ronnyfriedland.de>
2012-07-18 20:34:53 +0200

    checkstyle (tab/spaces)

--- src/main/java/de/ronnyfriedland/time/logic/EntityController.java
+++ src/main/java/de/ronnyfriedland/time/logic/EntityController.java
@@ -19,169 +19,169 @@ import de.ronnyfriedland.time.sort.SortParam;
  */
 public final class EntityController {
 
-	private static EntityController instance;
+    private static EntityController instance;
 
-	private final EntityManagerFactory emf;
-	private final EntityManager em;
+    private final EntityManagerFactory emf;
+    private final EntityManager em;
 
-	/**
-	 * Liefert eine Instanz von {@link EntityController}.
-	 * 
-	 * @return the {@link EntityController}
-	 */
-	public static EntityController getInstance() {
-		if (null == instance) {
-			instance = new EntityController();
-		}
-		return instance;
-	}
+    /**
+     * Liefert eine Instanz von {@link EntityController}.
+     * 
+     * @return the {@link EntityController}
+     */
+    public static EntityController getInstance() {
+        if (null == instance) {
+            instance = new EntityController();
+        }
+        return instance;
+    }
 
-	private EntityController() {
-		emf = Persistence.createEntityManagerFactory("timetable");
-		em = emf.createEntityManager();
-	}
+    private EntityController() {
+        emf = Persistence.createEntityManagerFactory("timetable");
+        em = emf.createEntityManager();
+    }
 
-	/**
-	 * Ermittelt einen Datensatz anhand der übergebenen UUID.
-	 * 
-	 * @param clazz
-	 *            Typ
-	 * @param uuid
-	 *            eindeutige ID des Datensatz
-	 * @return Datensatz
-	 */
-	public <T> T findById(final Class<T> clazz, final String uuid) {
-		return em.find(clazz, uuid);
-	}
+    /**
+     * Ermittelt einen Datensatz anhand der übergebenen UUID.
+     * 
+     * @param clazz
+     *            Typ
+     * @param uuid
+     *            eindeutige ID des Datensatz
+     * @return Datensatz
+     */
+    public <T> T findById(final Class<T> clazz, final String uuid) {
+        return em.find(clazz, uuid);
+    }
 
-	/**
-	 * TODO (Ronny Friedland): to be removed
-	 */
-	public <T> T findLast(final Class<T> clazz) {
-		T result;
-		TypedQuery<T> query = em.createQuery("SELECT e FROM " + clazz.getSimpleName()
-		        + " e ORDER BY e.lastModifiedDate desc", clazz);
-		query.setMaxResults(1);
-		try {
-			result = query.getSingleResult();
-		} catch (Exception e) {
-			result = null;
-		}
-		return result;
-	}
+    /**
+     * TODO (Ronny Friedland): to be removed
+     */
+    public <T> T findLast(final Class<T> clazz) {
+        T result;
+        TypedQuery<T> query = em.createQuery("SELECT e FROM " + clazz.getSimpleName()
+                + " e ORDER BY e.lastModifiedDate desc", clazz);
+        query.setMaxResults(1);
+        try {
+            result = query.getSingleResult();
+        } catch (Exception e) {
+            result = null;
+        }
+        return result;
+    }
 
-	public <T> Collection<T> findAll(final Class<T> clazz, final boolean includeDisabled) {
-		String queryString = "SELECT e FROM %1$s e ";
-		if (!includeDisabled) {
-			queryString += "WHERE e.enabled = 1 ";
-		}
-		TypedQuery<T> query = em.createQuery(String.format(queryString, clazz.getSimpleName()), clazz);
-		return query.getResultList();
-	}
+    public <T> Collection<T> findAll(final Class<T> clazz, final boolean includeDisabled) {
+        String queryString = "SELECT e FROM %1$s e ";
+        if (!includeDisabled) {
+            queryString += "WHERE e.enabled = 1 ";
+        }
+        TypedQuery<T> query = em.createQuery(String.format(queryString, clazz.getSimpleName()), clazz);
+        return query.getResultList();
+    }
 
-	public <T> Collection<T> findAll(final Class<T> clazz, final SortParam sortParam, final int max,
-	        final boolean includeDisabled) {
-		String queryString = "SELECT e FROM %1$s e ";
-		if (!includeDisabled) {
-			queryString += "WHERE e.enabled = 1 ";
-		}
-		queryString += "ORDER BY e.%2$s %3$s";
-		TypedQuery<T> query = em.createQuery(
-		        String.format(queryString, clazz.getSimpleName(), sortParam.getAttribute(), sortParam.getOrder()),
-		        clazz);
-		query.setMaxResults(max);
-		return query.getResultList();
-	}
+    public <T> Collection<T> findAll(final Class<T> clazz, final SortParam sortParam, final int max,
+            final boolean includeDisabled) {
+        String queryString = "SELECT e FROM %1$s e ";
+        if (!includeDisabled) {
+            queryString += "WHERE e.enabled = 1 ";
+        }
+        queryString += "ORDER BY e.%2$s %3$s";
+        TypedQuery<T> query = em.createQuery(
+                String.format(queryString, clazz.getSimpleName(), sortParam.getAttribute(), sortParam.getOrder()),
+                clazz);
+        query.setMaxResults(max);
+        return query.getResultList();
+    }
 
-	public <T> Collection<T> findAll(final Class<T> clazz, final SortParam sortParam, final boolean includeDisabled) {
-		String queryString = "SELECT e FROM %1$s e ";
-		if (!includeDisabled) {
-			queryString += "WHERE e.enabled = 1 ";
-		}
-		queryString += "ORDER BY e.%2$s %3$s";
-		TypedQuery<T> query = em.createQuery(
-		        String.format(queryString, clazz.getSimpleName(), sortParam.getAttribute(), sortParam.getOrder()),
-		        clazz);
-		return query.getResultList();
-	}
+    public <T> Collection<T> findAll(final Class<T> clazz, final SortParam sortParam, final boolean includeDisabled) {
+        String queryString = "SELECT e FROM %1$s e ";
+        if (!includeDisabled) {
+            queryString += "WHERE e.enabled = 1 ";
+        }
+        queryString += "ORDER BY e.%2$s %3$s";
+        TypedQuery<T> query = em.createQuery(
+                String.format(queryString, clazz.getSimpleName(), sortParam.getAttribute(), sortParam.getOrder()),
+                clazz);
+        return query.getResultList();
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T> Collection<T> findResultlistByParameter(final Class<T> clazz, final String namedQuery,
-	        final Map<String, Object> parameters) {
-		Query query = em.createNamedQuery(namedQuery);
-		if (null != parameters) {
-			for (Entry<String, Object> parameter : parameters.entrySet()) {
-				query.setParameter(parameter.getKey(), parameter.getValue());
-			}
-		}
-		return query.getResultList();
-	}
+    @SuppressWarnings("unchecked")
+    public <T> Collection<T> findResultlistByParameter(final Class<T> clazz, final String namedQuery,
+            final Map<String, Object> parameters) {
+        Query query = em.createNamedQuery(namedQuery);
+        if (null != parameters) {
+            for (Entry<String, Object> parameter : parameters.entrySet()) {
+                query.setParameter(parameter.getKey(), parameter.getValue());
+            }
+        }
+        return query.getResultList();
+    }
 
-	@SuppressWarnings("unchecked")
-	public <T> T findSingleResultByParameter(final Class<T> clazz, final String namedQuery,
-	        final Map<String, Object> parameters) {
-		Query query = em.createNamedQuery(namedQuery);
-		if (null != parameters) {
-			for (Entry<String, Object> parameter : parameters.entrySet()) {
-				query.setParameter(parameter.getKey(), parameter.getValue());
-			}
-		}
-		return (T) query.getSingleResult();
-	}
+    @SuppressWarnings("unchecked")
+    public <T> T findSingleResultByParameter(final Class<T> clazz, final String namedQuery,
+            final Map<String, Object> parameters) {
+        Query query = em.createNamedQuery(namedQuery);
+        if (null != parameters) {
+            for (Entry<String, Object> parameter : parameters.entrySet()) {
+                query.setParameter(parameter.getKey(), parameter.getValue());
+            }
+        }
+        return (T) query.getSingleResult();
+    }
 
-	public <T> int deleteAll(final Class<T> clazz) {
-		int updatedRows = 0;
-		TypedQuery<T> query = em.createQuery("DELETE FROM " + clazz.getSimpleName() + " e", clazz);
-		try {
-			em.getTransaction().begin();
-			query.executeUpdate();
-			em.getTransaction().commit();
-		} finally {
-			if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
-				em.getTransaction().rollback();
-			}
-		}
-		return updatedRows;
-	}
+    public <T> int deleteAll(final Class<T> clazz) {
+        int updatedRows = 0;
+        TypedQuery<T> query = em.createQuery("DELETE FROM " + clazz.getSimpleName() + " e", clazz);
+        try {
+            em.getTransaction().begin();
+            query.executeUpdate();
+            em.getTransaction().commit();
+        } finally {
+            if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
+                em.getTransaction().rollback();
+            }
+        }
+        return updatedRows;
+    }
 
-	public <T> void create(final T entity) {
-		try {
-			em.getTransaction().begin();
-			em.persist(entity);
-			em.getTransaction().commit();
-		} finally {
-			if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
-				em.getTransaction().rollback();
-			}
-		}
-	}
+    public <T> void create(final T entity) {
+        try {
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } finally {
+            if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
+                em.getTransaction().rollback();
+            }
+        }
+    }
 
-	public <T> void update(final T entity) {
-		try {
-			em.getTransaction().begin();
-			em.merge(entity);
-			em.getTransaction().commit();
-		} finally {
-			if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
-				em.getTransaction().rollback();
-			}
-		}
-	}
+    public <T> void update(final T entity) {
+        try {
+            em.getTransaction().begin();
+            em.merge(entity);
+            em.getTransaction().commit();
+        } finally {
+            if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
+                em.getTransaction().rollback();
+            }
+        }
+    }
 
-	public <T> void delete(final T entity) {
-		try {
-			em.getTransaction().begin();
-			em.remove(entity);
-			em.getTransaction().commit();
-		} finally {
-			if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
-				em.getTransaction().rollback();
-			}
-		}
+    public <T> void delete(final T entity) {
+        try {
+            em.getTransaction().begin();
+            em.remove(entity);
+            em.getTransaction().commit();
+        } finally {
+            if (em.getTransaction().isActive() && em.getTransaction().getRollbackOnly()) {
+                em.getTransaction().rollback();
+            }
+        }
 
-	}
+    }
 
-	public <T> void deleteDetached(final T entity) {
-		delete(em.merge(entity));
-	}
+    public <T> void deleteDetached(final T entity) {
+        delete(em.merge(entity));
+    }
 }
