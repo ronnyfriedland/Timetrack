0c299ba456a652bd1971b509cc8fa29c2400dc0c
Ronny Friedland <mail@ronnyfriedland.de>
2012-12-03 21:46:40 +0100

    refactoring liquibase

--- src/main/application/migrate.sh
+++ src/main/application/migrate.sh
@@ -1,1 +1,1 @@
-java -cp lib -jar lib/liquibase-core-1.9.5.jar --classpath="lib/derby-10.8.1.2.jar" --changeLogFile="db/master.xml" --defaultsFile="db/liquibase.properties" update
+java -cp lib -jar lib/liquibase-core-1.9.5.jar --classpath="lib/derby-10.8.1.2.jar" --changeLogFile="db/master.xml" --defaultsFile="db/liquibase.properties" --logLevel=info update
