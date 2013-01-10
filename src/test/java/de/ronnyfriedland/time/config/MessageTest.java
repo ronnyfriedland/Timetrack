55611ba6b33ac1c3f569773219e17cad450571b3
Ronny Friedland <mail@ronnyfriedland.de>
2013-01-10 20:45:38 +0100

    fix test

--- src/test/java/de/ronnyfriedland/time/config/MessageTest.java
+++ src/test/java/de/ronnyfriedland/time/config/MessageTest.java
@@ -6,14 +6,27 @@ import org.junit.Test;
 
 public class MessageTest {
 
-	private static final String[] PROPERTIES = new String[] { "test1", "test2" };
+    private static final Object[] TEXT_PROPERTIES = new Object[] { "test1", "test2" };
 
-	@Test
-	public void testMessageProperties() {
-		for (Messages message : Messages.values()) {
-			Assert.assertFalse(message.getMessage(PROPERTIES).startsWith("!"));
-			Assert.assertFalse(message.getMessage(PROPERTIES).endsWith("!"));
-		}
+    @Test
+    public void testMessageProperties() {
+        for (Messages message : Messages.values()) {
+            String formattedMessage = null;
+            switch (message) {
+            case EXPORT_DURATION:
+                formattedMessage = message.getMessage(new Integer(1), "test");
+                break;
+            case EXPORT_SUMMARY:
+                formattedMessage = message.getMessage(new Float(1), new Integer(2));
+                break;
+            default:
+                formattedMessage = message.getMessage(TEXT_PROPERTIES);
+                break;
+            }
+            Assert.assertNotNull(formattedMessage);
+            Assert.assertFalse(formattedMessage.startsWith("!"));
+            Assert.assertFalse(formattedMessage.endsWith("!"));
+        }
 
-	}
+    }
 }
