e8cfe5804ba81e15b7fd518aed4ab49f1db013ee
Ronny Friedland <mail@ronnyfriedland.de>
2013-01-10 21:38:46 +0100

    change version of liquibase

--- src/main/application/migrate.sh
+++ src/main/application/migrate.sh
@@ -1,1 +1,1 @@
-java -cp lib -jar lib/liquibase-core-1.9.5.jar --classpath="lib/derby-10.8.1.2.jar" --changeLogFile="db/master.xml" --defaultsFile="db/liquibase.properties" --logLevel=info update
+java -cp lib -jar lib/liquibase-core-2.0.5.jar --classpath="lib/derby-10.8.1.2.jar" --changeLogFile="db/master.xml" --defaultsFile="db/liquibase.properties" --logLevel=info update
