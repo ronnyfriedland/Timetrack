fa350a0efcb6fdbfdfee7d211318dd35ed95431b
Ronny Friedland <mail@ronnyfriedland.de>
2013-07-23 20:35:17 +0200

    fix migrate.bat + windows line breaks

--- src/main/application/migrate.bat
+++ src/main/application/migrate.bat
@@ -1,5 +1,8 @@
 @echo off
 
-IF not exist ${timetrack.path}/database/timetrack-database ( ren ${timetrack.path}/database/timetable-database ${timetrack.path}/database/timetrack-database )
-
+IF not exist data/database/timetrack-database (
+ xcopy data\database\timetable-database\* data\database\timetrack-database /i/s/q
+ rmdir data\database\timetable-database /s/q
+)
+ 
 java -cp lib -jar lib/liquibase-core-1.9.5.jar --classpath="lib/derby-10.8.1.2.jar" --changeLogFile="db/master.xml" --defaultsFile="db/liquibase.properties" --logLevel=info update
